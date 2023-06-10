0c91112676b37891c806a03d1b72ecbd046a65a7
FR-heitor <68114186+FR-heitor@users.noreply.github.com>
2023-06-10 11:41:29 -0300

    Update Congelamento.R

--- Congelamento.R
+++ Congelamento.R
@@ -19,7 +19,7 @@ View(CONGELAMENTO)
 Freeze <- CONGELAMENTO
 
 Freeze$LAGARTO <- as.factor(Freeze$ANIMAL)
-Freeze$COMPORTAMENTO <- as.factor(Freeze$Comportamento)
+Freeze$Dias <- as.factor(Freeze$Dias)
 Freeze$GRUPO <- as.factor(Freeze$GRUPO)
 Freeze$MEMORIA <- as.factor(Freeze$MEMORIA)
 
@@ -27,11 +27,11 @@ Freeze <- remove_missing(Freeze, na.rm = T)
 # Exposição
 # Passo 1: Modelo
 modelo_freeze <- aov_car(`C - EXP` ~ GRUPO*MEMORIA
-                         +Error(LAGARTO/(Comportamento)), 
+                         +Error(LAGARTO/(Dias)), 
                          data = Freeze)
 # Passo 2: ANOVA Robusta
 selvagem1 <- bwtrim(formula = `C - EXP` ~ GRUPO+MEMORIA, id = LAGARTO,
-         data = Freeze, tr = 0.2, nboot = 10000, MDIS = F, est = MOM) 
+         data = Freeze, tr = 0.1, nboot = 10000, MDIS = F, est = MOM) 
 selvagem1
 
 effectsize(modelo_freeze)
@@ -45,7 +45,7 @@ significant_results <- resumo$contrasts[p_values < 0.05,]
 # Ambientação
 # Passo 1: Modelo
 modelo_freeze <- aov_car(`C - AMB` ~ GRUPO*MEMORIA
-                         +Error(LAGARTO/(Comportamento)), 
+                         +Error(LAGARTO/(Dias)), 
                          data = Freeze)
 # Passo 2: ANOVA Robusta
 selvagem2 <- bwtrim(formula = `C - AMB` ~ GRUPO+MEMORIA,
@@ -64,12 +64,12 @@ significant_results <- resumo$contrasts[p_values < 0.05,]
 esquisse::esquisser(Freeze)
 
 
-Freeze$Comportamento <- factor(Freeze$Comportamento, levels = c("TREINO","TESTE"))
+Freeze$Comportamento <- factor(Freeze$Dias, levels = c("TREINO","TESTE"))
 
 library(ggplot2)
 library(ggbeeswarm)
 ggplot(Freeze) +
-  aes(y = MEMORIA, x = `C - EXP`, fill = Comportamento) +
+  aes(y = MEMORIA, x = `C - EXP`, fill = Dias) +
   geom_boxplot() +
   geom_beeswarm(dodge.width = 0.75) +
   scale_fill_manual(values = c(TREINO = "#FFFFFF", TESTE = "#464546")) +
@@ -81,7 +81,7 @@ ggplot(Freeze) +
   facet_wrap(vars(GRUPO), ncol = 2L)
 
 ggplot(Freeze) +
-  aes(y = MEMORIA, x = `C - AMB`, fill = Comportamento) +
+  aes(y = MEMORIA, x = `C - AMB`, fill = Dias) +
   geom_boxplot() +
   geom_beeswarm(dodge.width = 0.75) +
   scale_fill_manual(values = c(TREINO = "#FFFFFF", TESTE = "#464546")) +

1c77a9282a8a8bbcbed7d2eaaa0baed551891b28
FR-heitor <68114186+FR-heitor@users.noreply.github.com>
2023-06-11 17:00:52 -0300

    Update Analise_PCA.R

--- Analise_PCA.R
+++ Analise_PCA.R
@@ -1,8 +1,14 @@
 ####### final- PCA (Resumir variáveis em torno da medida) #################
+# Carregar bibliotecas
 library(readxl)
+library(FactoMineR)
+library(factoextra)
+library(tidyverse)
+
+# Carregar banco de dados
 PCA  <- read_excel("G://DADOS_ANALISE_PCA_LAGARTOS.xlsx", 
                    sheet = "PCA")
-
+# Definir variáveis independentes
 PCA$LAGARTO <- as.factor(PCA$LAGARTO)
 PCA$GRUPO <- as.factor(PCA$GRUPO)
 PCA$CODICAO <- as.factor(PCA$CODICAO)
@@ -12,75 +18,60 @@ PCA$INTERACAO <- as.factor(PCA$INTERACAO)
 
 PCA <- as.data.frame(PCA)
 
-res.pca <- PCA(PCA[,c(3:29)], scale.unit = T, ncp = 3,
-               quali.sup = c(1:5))
+# Realizando a Análise de Componentes Principais (PCA) nos dados das colunas 3 a 29. 
+# Os dados são escalonados para ter média 0 e desvio padrão 1.
+# Três dimensões principais são mantidas na análise. 
+# As colunas 1 a 5 são mantidas como variáveis suplementares.
+res.pca <- PCA(PCA[,c(3:29)], scale.unit = T, ncp = 3, quali.sup = c(1:5))
 
-grupo <- cbind(PCA[,1:5])
-grupo <- as.data.frame(grupo)
-
-#The proportion of variances retained by the different dimensions
+# Extraindo e visualizando os autovalores (variações) associados a cada componente principal.
 eig.val <- get_eigenvalue(res.pca)
 fviz_eig(res.pca)
 head(eig.val)
 
+# Extraindo os resultados para as variáveis na análise PCA.
 var <- get_pca_var(res.pca)
-var
 head(var$coord)
 head(var$contrib)
-
 head(var$cor)
 head(var$cos2)
 
-
-# Color variables by groups
+# Visualizando as variáveis da análise PCA.
+# As cores são determinadas pelo quadrado do cosseno das variáveis.
 fviz_pca_var(res.pca, col.var = "cos2", 
-             axes = c(1,2),
-             palette = c("ggplot2"),
-             legend.title = "cos2",
-             fill.var = "white",
-)
+             axes = c(1,2), 
+             palette = c("ggplot2"), 
+             legend.title = "cos2", 
+             fill.var = "white")
 
-# Create a grouping variable using kmeans
-# Create 3 groups of variables (centers = 3)
+# Agrupando as variáveis em 3 grupos usando o algoritmo k-means.
 set.seed(123)
 res.km <- kmeans(var$coord, centers = 3, nstart = 1)
 grp <- as.factor(res.km$cluster)
 
-
+# Extraindo os resultados para os indivíduos na análise PCA.
 ind <- get_pca_ind(res.pca)
-ind
 head(ind$coord)
 head(ind$contrib)
 head(ind$cos2)
 
+# Descrevendo as dimensões na análise PCA.
 res.desc <- dimdesc(res.pca, axes = c(1:3), proba = 0.05)
-# Description of dimension 1
-res.desc$Dim.1
-# Description of dimension 2
-res.desc$Dim.2
-# Description of dimension 3
-res.desc$Dim.3
 
-library("corrplot")
-col4 <- colorRampPalette(c("red", "white","blue"))
-corrplot(var$cor, is.corr=T, title = "Corelação das dimensões com os constructos", col = col4(150), tl.col = "black")
-corrplot(res.desc$Dim.1$quanti, is.corr=T, title = "Correlation DIM. 1")
-corrplot(res.desc$Dim.2$quanti, is.corr=T, title = "Correlation DIM. 2")
+# Visualizando a matriz de correlação entre as dimensões e os constructos.
+corrplot(var$cor, is.corr=T, 
+         title = "Corelação das dimensões com os constructos", 
+         col = col4(150), 
+         tl.col = "black")
 
-
-# Total cos2 of variables on Dim.1 until Dim.2
+# Visualizando o quadrado do cosseno das variáveis para as duas primeiras dimensões.
 fviz_cos2(res.pca, choice = "var", axes = 1)
 fviz_cos2(res.pca, choice = "var", axes = 2)
 
-fviz_pca_var(res.pca)
-# Contribution to the first dimension
+# Visualizando a contribuição de cada variável para as três primeiras dimensões.
 fviz_contrib(res.pca, "var", axes = 1)
-# Contribution to the second dimension
 fviz_contrib(res.pca, "var", axes = 2)
-# Contribution to the third dimension
 fviz_contrib(res.pca, "var", axes = 3)
-
-
 
 ############################## PLOT - PCA #################
 # Create the PCA biplot
@@ -90,7 +81,7 @@ fviz_pca_biplot(res.pca,
                 geom.ind = "point",
                 fill.ind = PCA$GRUPO, col.ind = 3, col.ind.sup = "black",
                 pointshape = 21, pointsize = 2,
-                palette = "ggplot2",#c("darkred","red","orange","yellow"),
+                palette = c("darkred","red","orange","yellow"),
                 addEllipses = T,
                 ellipse.type = "confidence",
                 # Variables

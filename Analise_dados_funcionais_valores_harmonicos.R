e9d8c996c3c49ed49593f411418b0fcc835cb03b
FR-heitor <68114186+FR-heitor@users.noreply.github.com>
2023-06-12 10:44:58 -0300

    Update Analise_dados_funcionais_valores_harmonicos.R

--- Analise_dados_funcionais_valores_harmonicos.R
+++ Analise_dados_funcionais_valores_harmonicos.R
@@ -1,12 +1,11 @@
 ################################### FDA (Comparação de anova fatorial) ####################################
 #bibliotecas
 library(readxl)
-library(afex)
 library(tidyverse)
 library(effectsize)
 library(emmeans)
 library(rstatix)
-library(ez)
+library(stats)
 
 #banco de dados
 Exemplo <- read_excel("G:/FDA.xlsx")
@@ -15,13 +14,16 @@ View(Exemplo)
 FDA <- Exemplo
 
 FDA$LAGARTO <- as.factor(FDA$LAGARTO)
-FDA$Grupo   <- as.factor(FDA$Grupo)     # CTL, MUS_1, MUS_2, MUS_3  combinados com Ambientação e Exposição      
+FDA$Grupo   <- as.factor(FDA$Grupo)     # CTL, MUS_1, MUS_2, MUS_3
 FDA$Dias    <- as.factor(FDA$Dias)      # TREINO e TESTE
+
+FDA$AF_1    <- as.numeric(FDA$AF_1)   # Exposição
+FDA$AF_2    <- as.numeric(FDA$AF_2)   # Ambientação
 
 # Filtra dados do treino
 FDA %>% filter (Dias == "TREINO") -> FDA_Tr
 # Passo 1: Modelo
-modelo_fda <- aov(AF1_CL ~ Grupo, data = FDA_Tr)
+modelo_fda <- aov(AF1_CL ~ Grupo, data = FDA_Tr) # Adaptar para Exposição ou Ambientação - aqui está em Exposição (AF_1)
 # Passo 2: ANOVA mista
 Resultado_fda <- anova_test(modelo_fda)
 Resultado_fda
@@ -35,10 +37,10 @@ p_values <- resumo$contrasts[, "p.value"]
 # Filtro dos resultados p-valor < 0.05
 significant_results <- resumo$contrasts[p_values < 0.05,]
 
-# Filtra dados do treino
-FDA %>% filter (Dias == "TREINO") -> FDA_Tt
+# Filtra dados do teste
+FDA %>% filter (Dias == "TESTE") -> FDA_Tt
 # Passo 1: Modelo
-modelo_fda <- aov(AF2_CL ~ Grupo, data = FDA_Tt)
+modelo_fda <- aov(AF1_CL ~ Grupo, data = FDA_Tt)  # Adaptar para Exposição ou Ambientação - aqui está em Exposição (AF_1)
 # Passo 2: ANOVA mista
 Resultado_fda <- anova_test(modelo_fda)
 Resultado_fda
